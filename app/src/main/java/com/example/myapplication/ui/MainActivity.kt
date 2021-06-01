1083aa593892bcdb0ef80a4e1c9dcaa3c66bcb0f
JaeWonKim <turitus@naver.com>
2021-06-01 22:42:06 +0900

    ⚡️ :: [LoginPage] 로그인 프래그먼트 생성

--- app/src/main/java/com/example/myapplication/ui/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.myapplication
+package com.example.myapplication.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
