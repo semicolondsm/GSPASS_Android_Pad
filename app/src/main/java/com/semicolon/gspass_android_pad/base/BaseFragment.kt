246ed105e78851304a40adc54af8a5508ec09699
JaeWonKim <turitus@naver.com>
2021-06-02 22:51:28 +0900

    ⚡️:: BaseFragment 생성&사용

---
+++ app/src/main/java/com/semicolon/gspass_android_pad/base/BaseFragment.kt
@@ -0,0 +1,4 @@
+package com.semicolon.gspass_android_pad.base
+
+class BaseFragment {
+}
--- app/src/main/java/com/semicolon/gspass_android_pad/ui/login/LoginFragment.kt
+++ app/src/main/java/com/semicolon/gspass_android_pad/ui/login/LoginFragment.kt
@@ -6,14 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.semicolon.gspass_android_pad.R
+import com.semicolon.gspass_android_pad.base.BaseFragment
+import com.semicolon.gspass_android_pad.databinding.FragmentLoginBinding
+import com.semicolon.gspass_android_pad.viewmodel.LoginViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class LoginFragment : Fragment() {
+class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    private val vm : LoginViewModel by viewModel()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = this
+        binding.vm = vm
     }
 }
