b985e4cf451f1b42d8e155698ba5157544f01444
JaeWonKim <turitus@naver.com>
2021-06-02 23:49:12 +0900

    📑:: [AddSchool]학교 추가 fragment 생성

---
+++ app/src/main/java/com/semicolon/gspass_android_pad/ui/login/AddSchoolFragment.kt
@@ -0,0 +1,18 @@
+package com.semicolon.gspass_android_pad.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.semicolon.gspass_android_pad.R
+
+class AddSchoolFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_school, container, false)
+    }
+}
