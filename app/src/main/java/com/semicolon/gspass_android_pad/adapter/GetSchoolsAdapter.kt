265165fc16e5ac74ff25cbe3db22637b1bb561d4
JaeWonKim <turitus@naver.com>
2021-06-03 17:11:41 +0900

    ⚡️:: [AddSchool] 학교 확인 recyclerview adapter

---
+++ app/src/main/java/com/semicolon/gspass_android_pad/adapter/GetSchoolsAdapter.kt
@@ -0,0 +1,38 @@
+package com.semicolon.gspass_android_pad.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.lifecycle.LiveData
+import androidx.recyclerview.widget.RecyclerView
+import com.semicolon.gspass_android_pad.databinding.ItemSearchSchoolBinding
+import com.semicolon.gspass_android_pad.model.GetSchoolResponse
+
+class GetSchoolsAdapter : RecyclerView.Adapter<GetSchoolsAdapter.SchoolViewHolder>() {
+
+    private var schools = ArrayList<GetSchoolResponse>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GetSchoolsAdapter.SchoolViewHolder {
+        val binding =
+            ItemSearchSchoolBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return SchoolViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: SchoolViewHolder, position: Int) =
+        holder .bind(schools[position])
+
+    override fun getItemCount(): Int =
+        schools.size
+
+    fun setItems(schoolsList:LiveData<ArrayList<GetSchoolResponse>>){
+        this.schools = schoolsList.value!!
+        notifyDataSetChanged()
+    }
+
+    class SchoolViewHolder(private val binding: ItemSearchSchoolBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(schoolMode: GetSchoolResponse) {
+            binding.model = schoolMode
+        }
+    }
+}
--- app/src/main/java/com/semicolon/gspass_android_pad/viewmodel/AddSchoolViewModel.kt
+++ app/src/main/java/com/semicolon/gspass_android_pad/viewmodel/AddSchoolViewModel.kt
@@ -1,12 +1,17 @@
 package com.semicolon.gspass_android_pad.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.semicolon.gspass_android_pad.data.remote.login.LoginApiProvider
+import com.semicolon.gspass_android_pad.model.GetSchoolResponse
 
 class AddSchoolViewModel(private val loginApiProvider: LoginApiProvider) : ViewModel() {
 
     val schoolName = MutableLiveData<String>()
+
+    private val _schools = MutableLiveData<List<GetSchoolResponse>>()
+    val school : LiveData<List<GetSchoolResponse>> get() = _schools
 
     fun loadSchools() {
         loginApiProvider.getSchools(schoolName.value!!).subscribe({response->
