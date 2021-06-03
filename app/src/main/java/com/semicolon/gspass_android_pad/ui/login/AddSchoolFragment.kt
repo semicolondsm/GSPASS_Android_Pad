3b0aabeb2a22e827c307decb53f6ff7b3bf472e7
JaeWonKim <turitus@naver.com>
2021-06-03 23:07:43 +0900

    ⚡️:: textWatcher 생성

--- app/src/main/java/com/semicolon/gspass_android_pad/ui/login/AddSchoolFragment.kt
+++ app/src/main/java/com/semicolon/gspass_android_pad/ui/login/AddSchoolFragment.kt
@@ -1,6 +1,8 @@
 package com.semicolon.gspass_android_pad.ui.login
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +12,7 @@ import com.semicolon.gspass_android_pad.adapter.GetSchoolsAdapter
 import com.semicolon.gspass_android_pad.base.BaseFragment
 import com.semicolon.gspass_android_pad.databinding.FragmentAddSchoolBinding
 import com.semicolon.gspass_android_pad.viewmodel.AddSchoolViewModel
+import io.reactivex.rxjava3.disposables.Disposable
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class AddSchoolFragment : BaseFragment<FragmentAddSchoolBinding>(R.layout.fragment_add_school) {
@@ -23,5 +26,25 @@ class AddSchoolFragment : BaseFragment<FragmentAddSchoolBinding>(R.layout.fragme
         binding.lifecycleOwner = this
         binding.adapter = adapter
         binding.vm = vm
+    }
+
+    private val textWatcher = object : TextWatcher{
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+
+        }
+
+    }
+
+    
+    private fun observeInputText(){
+
     }
 }
