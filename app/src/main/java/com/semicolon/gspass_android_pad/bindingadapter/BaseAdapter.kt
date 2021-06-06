196086a69cfa78729b9362f38d6d539e66267378
JaeWonKim <turitus@naver.com>
2021-06-06 14:48:27 +0900

    ⚰️:: BaseBindingAdapter

--- app/src/main/java/com/semicolon/gspass_android_pad/bindingadapter/BaseAdapter.kt
+++
@@ -1,9 +0,0 @@
-package com.semicolon.gspass_android_pad.bindingadapter
-
-import androidx.databinding.BindingAdapter
-import androidx.recyclerview.widget.RecyclerView
-
-@BindingAdapter("adapter")
-fun RecyclerView.adapter(setAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>) {
-    adapter = setAdapter
-}
