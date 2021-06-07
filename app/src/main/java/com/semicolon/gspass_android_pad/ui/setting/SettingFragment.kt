418c90bf014d5ca21ea868362b4e51b1e57d192e
JaeWonKim <turitus@naver.com>
2021-06-07 23:50:43 +0900

    📑:: 세팅 프래그먼트 생성

---
+++ app/src/main/java/com/semicolon/gspass_android_pad/ui/setting/SettingFragment.kt
@@ -0,0 +1,16 @@
+package com.semicolon.gspass_android_pad.ui.setting
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.semicolon.gspass_android_pad.R
+import com.semicolon.gspass_android_pad.base.BaseFragment
+import com.semicolon.gspass_android_pad.databinding.FragmentSettingBinding
+
+class SettingFragment : BaseFragment<FragmentSettingBinding>(R.layout.fragment_setting) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
