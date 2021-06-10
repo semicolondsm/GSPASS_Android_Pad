b82ee3798288116ff365f6e68fe23ec7656838fc
JaeWonKim <turitus@naver.com>
2021-06-10 22:08:15 +0900

    📑:: [QrCode]viewmodel

--- app/src/main/java/com/semicolon/gspass_android_pad/ui/QrCodeActivity.kt
+++ app/src/main/java/com/semicolon/gspass_android_pad/ui/QrCodeActivity.kt
@@ -3,7 +3,7 @@ package com.semicolon.gspass_android_pad.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class QrCodeActivity : AppCompatActivity() {
+class QrCodeActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qr_code)
